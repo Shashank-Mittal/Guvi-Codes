f70dab8728c2c5f78127e49bd8fd805d73536e79
SHASHANK MITTAL <39668339+Shashank-Mittal@users.noreply.github.com>
2019-01-13 21:57:22 +0530

    Update positiveornegetive.py

--- positiveornegetive.py
+++ positiveornegetive.py
@@ -1,7 +1,10 @@
-number=int(input())
-if(number>0):
-	print("Positive")
-elif(number<0):
-	print("Negetive")
-else:
-	print("Zero")
+try:
+	number=int(input())
+	if(number>0):
+		print("Positive")
+	elif(number<0):
+		print("Negetive")
+	else:
+		print("Zero")
+except:
+	print("Invalid Input")
