36ce9e56867b2747b632a2f048372e1b01ac8432
SHASHANK MITTAL <39668339+Shashank-Mittal@users.noreply.github.com>
2019-01-16 16:36:34 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Hello
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int Number = input.nextInt();
+		for(int i=0;i<Number;i++)
+		System.out.println("Hello");  
+		
+	}
+}
