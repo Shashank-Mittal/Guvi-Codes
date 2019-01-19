42eae62e44a604f0ce82ca9ed71d33ae730d8b44
SHASHANK MITTAL <39668339+Shashank-Mittal@users.noreply.github.com>
2019-01-19 15:50:46 +0530

    Update CamelCase.py

--- Player/Set_1/CamelCase.py
+++ Player/Set_1/CamelCase.py
@@ -1,3 +1,10 @@
 String = input()
 String = String.split(" ")
-print(String)
+temp=""
+try:
+	for i in range (0, len(String)):
+		temp = String[i][0].upper()
+		String[i] = temp + String[i][1::]
+	print(" ".join(String))
+except:
+	print("Invalid")
