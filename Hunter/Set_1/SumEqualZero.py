01dfaa7b463315dfff54964f2d3b20c430ef5134
SHASHANK MITTAL <39668339+Shashank-Mittal@users.noreply.github.com>
2019-03-09 19:37:46 +0530

    Update SumEqualZero.py

--- Hunter/Set_1/SumEqualZero.py
+++ Hunter/Set_1/SumEqualZero.py
@@ -1,14 +1,17 @@
 try:
 	number = int(input())
 	string = list(map(int, input().split(" ")))
-	for i in string:
-		k = 0-i
-		check = string.count(k)
-		if(check>0):
-			print(str(i)+" "+str(k))
-			k="Yes"
-			break
-	if(k!="Yes"):
-		print("There is no two element whose sum is equal or closest to zero")
+	check=0
+	mini = 10000000000
+	for i in range(0,len(string)-1):
+		for j in range(i+1,len(string)):
+			check = string[i] + string[j]
+			if(check<0):
+				check=-(check)
+			if(check<mini):
+				mini=check
+				k=i
+				p=j
+	print(str(string[k])+" "+str(string[p]))
 except:
 	print("Invalid input")
